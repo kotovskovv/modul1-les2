c1db5ff58a2b1f681e0ed1312695f4abb9dc1137
kotovskovv <kotovskovv@bk.ru>
2024-06-23 19:43:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+number_of_completed_tasks = 12
+number_of_hours_spent = 1.5
+course_name = 'Pyhton'
+time_spent_on_1_task = (number_of_hours_spent/number_of_completed_tasks)
+print('Курс:',course_name,","'Всего задач:',number_of_completed_tasks,',затрачено часов:',number_of_hours_spent,',','среднее время выполнения',time_spent_on_1_task,'часа.')
